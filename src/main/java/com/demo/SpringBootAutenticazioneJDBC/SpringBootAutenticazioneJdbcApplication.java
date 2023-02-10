5df6e45e655be5fdfbb05ffc73e169e95b80195f
Michael <michaelspig@gmail.com>
2023-02-10 16:48:38 +0100

    Init ProjectSpringBoot

---
+++ src/main/java/com/demo/SpringBootAutenticazioneJDBC/SpringBootAutenticazioneJdbcApplication.java
@@ -0,0 +1,15 @@
+package com.demo.SpringBootAutenticazioneJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutenticazioneJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAutenticazioneJdbcApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/demo/SpringBootAutenticazioneJDBC/SpringBootAutenticazioneJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.SpringBootAutenticazioneJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutenticazioneJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
